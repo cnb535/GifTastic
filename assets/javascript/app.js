af96e7e7d5a25fd1dc511279f023021ddedd9140
Christina Bertram <Jamerican92grl@MacBook-Pro.home>
2018-04-25 20:37:00 -0400

    AJAX requests are working and the gifs are being added to the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-const topics = ["Bob's Burgers", "Family Guy", "New Girl", "Blackish", "Broad City", "Saturday Night Live", "Insecure", "Napoleon Dynamite", "22 Jump Street", "The Heat"];
+const topics = ["Bob's Burgers", "Family Guy", "New Girl", "Blackish", "Broad City", "Saturday Night Live", "Insecure", "Napoleon Dynamite", "22 Jump Street", "Mean Girls"];
 
 for (let i = 0; i < topics.length; i++) {
 	const topicBtn = $("<button>");
@@ -10,4 +10,42 @@ for (let i = 0; i < topics.length; i++) {
 	topicBtn.text(topics[i]);
 
 	$("#btns").append(topicBtn);
-};
+}
+
+const APIKey = "u6mBkPzGeHf1sNS42hLwSYkGRQ2PY04c";
+
+$("button").on("click", function() {
+	
+	const title = $(this).attr("title");
+
+	console.log(title);
+
+	const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + title + "&api_key=" + APIKey + "&limit=10";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.done(function(response) {
+		const results = response.data;
+
+		for (let i = 0; i < results.length; i++) {
+			if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+				const gifDiv = $("<div class='item'>");
+
+				const rating = results[i].rating;
+
+				const r = $("<p>").text("Rating: " + rating);
+
+				const gifImage = $("<img>");
+
+				gifImage.attr("src", results[i].images.fixed_height.url);
+
+				gifDiv.append(r);
+				gifDiv.append(gifImage);
+
+				$("#selection").prepend(gifDiv);
+			}
+		}
+	});
+});
